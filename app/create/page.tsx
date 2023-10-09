cad1f85614fb4dc91e99732a5f8490041e22c45e
Christian Chiarulli <chris.machine@pm.me>
2023-10-09 00:24:37 -0400

    publishing works now

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -5,8 +5,13 @@ import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
 import MarkdownIt from "markdown-it";
-// import "react-markdown-editor-lite/lib/index.css";
-import './markdown-editor.css'
+import { getEventHash } from "nostr-tools";
+import type { Event } from "nostr-tools";
+
+import { createUniqueUrl } from "../lib/utils";
+import { useRelayStore } from "../stores/relayStore";
+import { useUserProfileStore } from "../stores/userProfileStore";
+import "./markdown-editor.css";
 
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
@@ -15,17 +20,57 @@ const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
 });
 
 export default function CreateBounty() {
-  // Declare a state variable for the text input
-  const [inputValue, setInputValue] = useState("");
+  const { publish, relayUrl } = useRelayStore();
+  const { getUserPublicKey } = useUserProfileStore();
 
-  // Handler for the input change event
-  const handleInputChange = (event: any) => {
-    setInputValue(event.target.value);
+  const [title, setTitle] = useState("");
+  const [reward, setReward] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (event: any) => {
+    setTitle(event.target.value);
+  };
+
+  const handleRewardChange = (event: any) => {
+    setReward(event.target.value);
   };
 
   function handleEditorChange({ html, text }: any) {
-    console.log("handleEditorChange", html, text);
+    setContent(text);
   }
+
+  const handlePublish = async () => {
+    console.log("handlePublish", title, reward, content);
+
+    const uniqueUrl = createUniqueUrl(title);
+
+    // TODO: handle tags
+    const initialTags: Array<any> = [];
+
+    const additionaltags = [
+      ["d", uniqueUrl],
+      ["title", title],
+      ["status", "open"],
+      ["value", reward],
+    ];
+
+    const tags = initialTags.concat(additionaltags);
+
+    let event: Event = {
+      id: "",
+      sig: "",
+      kind: 30050,
+      created_at: Math.floor(Date.now() / 1000),
+      tags: tags,
+      content: content,
+      pubkey: getUserPublicKey(),
+    };
+
+    event.id = getEventHash(event);
+    event = await window.nostr.signEvent(event);
+
+    publish([relayUrl], event);
+  };
 
   return (
     <div className="lg:pl-72">
@@ -36,12 +81,12 @@ export default function CreateBounty() {
             <h2 className="text-gray-100">Title</h2>
             <input
               type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2"
+              value={title}
+              onChange={handleTitleChange}
+              className="mt-4 w-full rounded border border-gray-600 bg-gray-700 p-2 text-gray-100"
               placeholder="Bounty title"
             />
-            <h2 className="pt-8 pb-4 text-gray-100">Content</h2>
+            <h2 className="pb-4 pt-8 text-gray-100">Content</h2>
             <MdEditor
               config={{
                 view: {
@@ -58,24 +103,28 @@ export default function CreateBounty() {
               onChange={handleEditorChange}
             />
 
-            <h2 className="text-gray-100 pt-8">Reward (sats)</h2>
+            <h2 className="pt-8 text-gray-100">Reward (sats)</h2>
             <input
               type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2"
+              value={reward}
+              onChange={handleRewardChange}
+              className="mt-4 w-full rounded border border-gray-600 bg-gray-700 p-2 text-gray-100"
               placeholder="Value in sats"
             />
-            <h2 className="text-gray-100 pt-8">Tags</h2>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2"
-              placeholder="Comma separated tags"
-            />
+            {/* TODO: handle tags */}
+            {/* <h2 className="text-gray-100 pt-8">Tags</h2> */}
+            {/* <input */}
+            {/*   type="text" */}
+            {/*   value={inputValue} */}
+            {/*   onChange={handleInputChange} */}
+            {/*   className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2" */}
+            {/*   placeholder="Comma separated tags" */}
+            {/* /> */}
             <div className="mt-6 flex items-center justify-between">
-              <button className="flex items-center gap-x-2 rounded-lg bg-blue-700 px-3 py-2 text-sm font-medium text-gray-200">
+              <button
+                className="flex items-center gap-x-2 rounded-lg bg-blue-700 px-3 py-2 text-sm font-medium text-gray-200"
+                onClick={handlePublish}
+              >
                 Create Bounty
               </button>
             </div>
