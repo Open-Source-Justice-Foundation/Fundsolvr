2459f07196349e604eb86a060b43631e20a5d653
mahcodes <mhmdali102@bk.ru>
2023-10-11 15:52:44 +0300

    messages concept

---
+++ app/messages/components/Chat.tsx
@@ -0,0 +1,113 @@
+import { Fragment } from "react";
+
+import { classNames } from "@/app/lib/utils";
+import { Menu, Transition } from "@headlessui/react";
+import { SpeakerXMarkIcon, TrashIcon, ClipboardIcon } from "@heroicons/react/24/outline";
+
+import Badge from "./Badge";
+
+export interface IUser {
+  img: string;
+  username: string;
+  unread: boolean;
+  bio: string;
+}
+
+interface IChatProps {
+  user: IUser;
+}
+
+export default function Chat({ user }: IChatProps) {
+  const { username, img, unread } = user;
+  return (
+    <div className="rounded-md border border-transparent p-4 text-white hover:bg-gray-800">
+      <div className="group flex items-center gap-4">
+        <div className="relative h-12 w-12">
+          <img src={img} alt={username} className="aspect-square w-full rounded-full" />
+          <svg
+            className="absolute bottom-1 right-1 w-6 translate-x-1/2 translate-y-1/2"
+            viewBox="-4 0 27 19"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              className="fill-sky-500"
+              d="M7.844 17.563c1.039 1.046 2.031 1.039 3.078 0l1.172-1.172c.11-.11.203-.141.344-.141h1.656c1.476 0 2.18-.703 2.18-2.18v-1.656c0-.14.038-.242.14-.344l1.172-1.18c1.047-1.038 1.039-2.03 0-3.07l-1.172-1.172a.454.454 0 0 1-.14-.343V4.648c0-1.476-.704-2.18-2.18-2.18h-1.656a.443.443 0 0 1-.344-.14l-1.172-1.172C9.875.11 8.882.11 7.844 1.164L6.672 2.328a.443.443 0 0 1-.344.14H4.672c-1.477 0-2.18.688-2.18 2.18v1.657c0 .14-.039.242-.14.343L1.18 7.82c-1.047 1.04-1.04 2.032 0 3.07l1.172 1.18c.101.102.14.203.14.344v1.656c0 1.477.703 2.18 2.18 2.18h1.656c.14 0 .234.031.344.14l1.172 1.172Zm.242-4.204a.883.883 0 0 1-.664-.28l-2.5-2.798a.778.778 0 0 1-.203-.531c0-.469.336-.805.82-.805.266 0 .461.086.633.274l1.883 2.101 3.765-5.375c.188-.265.39-.375.703-.375.485 0 .829.336.829.79a.936.936 0 0 1-.18.515l-4.383 6.148a.831.831 0 0 1-.703.336Z"
+            />
+          </svg>
+        </div>
+        <div className="flex flex-1 flex-col gap-2">
+          <span className="text-md font-bold">{username}</span>
+          <p className={classNames("flex items-center gap-2", unread ? "text-white" : "text-gray-400")}>
+            <span>You:</span>
+            hello from dustinbrett.com
+            <span>·</span>
+            <span>42m</span>
+          </p>
+        </div>
+        {unread && <Badge size={2} />}
+        <Menu as="div" className="relative inline-block">
+          <Menu.Button className="invisible rounded p-1 transition-all hover:bg-gray-700 group-focus-within:visible group-hover:visible">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              width="24"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+              />
+            </svg>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute z-10 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-600 rounded-md border border-gray-600 bg-gray-800 text-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button className={`${active ? "bg-gray-700" : ""} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}>
+                      <ClipboardIcon className="h-6 w-6" />
+                      <span>Copy npub</span>
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button className={`${active ? "bg-gray-700" : ""} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}>
+                      <SpeakerXMarkIcon className="h-6 w-6" />
+                      <span>Mute</span>
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${active ? "bg-red-500/20" : ""
+                        } group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm text-red-500`}
+                    >
+                      <TrashIcon className="h-6 w-6" />
+                      <span>Delete</span>
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+    </div>
+  );
+}
--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,7 +1,96 @@
-export default function Messages() {
+"use client";
+
+import { Tab } from "@headlessui/react";
+import { PlusIcon } from "@heroicons/react/20/solid";
+
+import { classNames } from "../lib/utils";
+import Chat, { IUser } from "./components/Chat";
+import Badge from "./components/Badge";
+
+const USERS:IUser[] = [
+  {
+    img: "https://www.chrisatmachine.com/assets/headshot.jpg",
+    username: "Christian Chiarulli",
+    unread: true,
+    bio: "✨ Designing, building and talking about digital products.",
+  },
+  {
+    img: "https://cdnb.artstation.com/p/assets/images/images/043/120/123/large/wizix-nakamoto-master-full.jpg?1636383169",
+    username: "satoshi",
+    unread: true,
+    bio: "✨ Designing, building and talking about digital products.",
+  },
+  {
+    img: "https://pfp.nostr.build/6838p.jpeg",
+    username: "Snowden",
+    unread: false,
+    bio: "✨ Designing, building and talking about digital products.",
+  },
+  {
+    img: "https://void.cat/d/MreaerC65YkE8zeHvVv6XM.webp",
+    username: "NVK",
+    unread: false,
+    bio: "✨ Designing, building and talking about digital products.",
+  },
+  {
+    img: "https://imgproxy.coracle.social/x/s:280:280/aHR0cHM6Ly9maWF0amFmLmNvbS9zdGF0aWMvZmF2aWNvbi5qcGc=",
+    username: "fiatjaf",
+    unread: false,
+    bio: "✨ Designing, building and talking about digital products.",
+  },
+];
+
+const TABS = [
+  {
+    title: "Conversations",
+    unread: 2,
+  },
+  {
+    title: "Requests",
+    unread: 1,
+  },
+];
+
+export default function MessagesPage() {
   return (
-    <div>
-      messages
+    <div className="pb-20 pt-10">
+      <div className="mx-auto flex h-screen w-full items-start justify-center text-sm text-gray-900 antialiased dark:text-white">
+        <div className="mx-auto w-full max-w-3xl rounded-md border border-gray-600 p-4">
+          <Tab.Group>
+            <Tab.List className="flex items-center border-b border-b-gray-600">
+              {TABS.map(({ title, unread }) => (
+                <Tab
+                  key={title}
+                  className={({ selected }) =>
+                    classNames(`rounded-t-md border-x border-t p-4 flex items-center gap-2`, selected ? "border-gray-600 focus:outline-none" : "border-transparent")
+                  }
+                >
+                  <span>{title}</span>
+                  <Badge content={unread} />
+                </Tab>
+              ))}
+              <div className="flex flex-1 justify-end">
+                <button className="flex items-center gap-x-2 rounded-full bg-indigo-600 px-4 py-2 font-medium text-white hover:bg-indigo-500">
+                  <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                  New Message
+                </button>
+              </div>
+            </Tab.List>
+            <Tab.Panels className="mt-4 flex list-none flex-col gap-4">
+              <Tab.Panel>
+                {USERS.map((user, idx) => (
+                  <Chat key={idx} user={user} />
+                ))}
+              </Tab.Panel>
+              <Tab.Panel>
+                {USERS.slice(1, 3).map((user, idx) => (
+                  <Chat key={idx} user={user} />
+                ))}
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
