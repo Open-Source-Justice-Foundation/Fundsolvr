9c4906f2546c75d19a9cdba38ec19d50fdc2efce
Brian <6819987+brianjcarroll@users.noreply.github.com>
2023-10-24 00:04:19 -0400

    feat: Add user profile settings (#30)

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import { type Event, getEventHash } from "nostr-tools";
+import { type Event, Filter, getEventHash } from "nostr-tools";
 import { Octokit } from "octokit";
 
 import { useRelayStore } from "../stores/relayStore";
@@ -11,14 +11,39 @@ import { useUserProfileStore } from "../stores/userProfileStore";
 
 export default function Settings() {
   const { getUserPublicKey, getUserEvent, getUserProfile } = useUserProfileStore();
-  const { publish, relayUrl } = useRelayStore();
+  const { userPublicKey } = useUserProfileStore();
+  const { publish, subscribe, relayUrl } = useRelayStore();
 
   const [gistId, setGistId] = useState("");
   const [github, setGithub] = useState("");
 
+  const [imageURL, setImageUrl] = useState("");
+  const [username, setUsername] = useState("");
+  const [about, setAbout] = useState("");
+  const [currentUserEvent, setCurrentUserEvent] = useState<Event>({
+    kind: 0,
+    tags: [],
+    content: "",
+    created_at: 0,
+    pubkey: "",
+    id: "",
+    sig: "",
+  });
+
   const handleGistIdChange = (event: any) => {
     setGistId(event.target.value);
   };
+
+  const userFilter: Filter = {
+    kinds: [0],
+    authors: [getUserPublicKey()],
+  };
+
+  interface Metadata {
+    name: string;
+    picture: string;
+    about: string;
+  }
 
   const addGithubProfile = async (username: String) => {
     const userProfileEvent = getUserEvent();
@@ -81,11 +106,61 @@ export default function Settings() {
     }
   }
 
+  const getUserMetadata = async () => {
+    let userProfileEvent: Event;
+    const onEvent = (event: Event) => {
+      userProfileEvent = event;
+    };
+
+    const onEOSE = () => {
+      const parsedUserProfile: Metadata = JSON.parse(userProfileEvent.content);
+      setCurrentUserEvent(userProfileEvent);
+      setMetadata(parsedUserProfile);
+    };
+    subscribe([relayUrl], userFilter, onEvent, onEOSE);
+  };
+
+  const setMetadata = (metadata: Metadata) => {
+    const { name, picture, about } = metadata;
+    if (picture) {
+      setImageUrl(picture);
+    }
+    if (about) {
+      setAbout(about);
+    }
+    if (name) {
+      setUsername(name);
+    }
+  };
+
+  const saveMetadata = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    const currentContent = JSON.parse(currentUserEvent.content);
+    const updatedUserProfile = JSON.stringify({ ...currentContent, name: username, picture: imageURL, about });
+    let event: Event = {
+      id: "",
+      sig: "",
+      kind: 0,
+      created_at: Math.floor(Date.now() / 1000),
+      tags: currentUserEvent?.tags || [],
+      content: updatedUserProfile,
+      pubkey: getUserPublicKey(),
+    };
+    event = await window.nostr.signEvent(event);
+    publish([relayUrl], event);
+  };
+
   useEffect(() => {
     const userProfile = getUserProfile(relayUrl);
     if (userProfile) {
       setGithub(userProfile.github);
+      setMetadata({
+        name: userProfile.name,
+        picture: userProfile.picture,
+        about: userProfile.about,
+      });
     }
+    getUserMetadata();
   }, []);
 
   // TODO: redo the whole thing
@@ -106,7 +181,12 @@ export default function Settings() {
                   Photo
                 </label>
                 <div className="mt-2 flex items-center gap-x-3">
-                  <UserCircleIcon className="h-12 w-12 text-gray-300 dark:text-gray-600" aria-hidden="true" />
+                  {imageURL ? (
+                    <img className="inline-block h-12 w-12 rounded-full" src={imageURL} alt="" />
+                  ) : (
+                    <UserCircleIcon className="h-12 w-12 text-gray-300 dark:text-gray-600" aria-hidden="true" />
+                  )}
+
                   <div className="flex w-full max-w-sm rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 dark:ring-gray-600">
                     <input
                       type="text"
@@ -114,6 +194,10 @@ export default function Settings() {
                       id="picture"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-gray-700 dark:text-gray-100 dark:ring-gray-600 sm:text-sm sm:leading-6"
                       placeholder="image URL"
+                      value={imageURL}
+                      onChange={(e) => {
+                        setImageUrl(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
@@ -129,8 +213,12 @@ export default function Settings() {
                       name="username"
                       id="username"
                       autoComplete="username"
-                      className="block w-full rounded-md border-0 dark:bg-gray-700 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:text-gray-100 dark:ring-gray-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-gray-700 dark:text-gray-100 dark:ring-gray-600 sm:text-sm sm:leading-6"
                       placeholder="satoshi"
+                      value={username}
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
                     />
                   </div>
                 </div>
@@ -146,7 +234,10 @@ export default function Settings() {
                     name="about"
                     rows={3}
                     className="block w-full max-w-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-gray-700 dark:text-gray-100 dark:ring-gray-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
+                    value={about}
+                    onChange={(e) => {
+                      setAbout(e.target.value);
+                    }}
                   />
                 </div>
                 <p className="mt-3 text-sm leading-6 text-gray-600 dark:text-gray-300">Write a few sentences about yourself.</p>
@@ -182,7 +273,7 @@ export default function Settings() {
             Cancel
           </button>
           <button
-            type="submit"
+            onClick={saveMetadata}
             className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-500"
           >
             Save
