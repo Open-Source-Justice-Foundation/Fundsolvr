c669c0eff78be6f9e75012f8acbaa837c215712b
Christian Chiarulli <chris.machine@pm.me>
2023-10-25 01:58:32 -0400

    feat: must be logged in to create bounty

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -25,7 +25,7 @@ const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
 
 export default function CreateBounty() {
   const { publish, relayUrl } = useRelayStore();
-  const { getUserPublicKey } = useUserProfileStore();
+  const { getUserPublicKey, userPublicKey } = useUserProfileStore();
   const { setCachedBountyEvent, getBountyEvents, setBountyEvents } = useBountyEventStore();
 
   // TODO: use this
@@ -89,8 +89,10 @@ export default function CreateBounty() {
       created_at: Math.floor(Date.now() / 1000),
       tags: tags,
       content: content,
-      pubkey: getUserPublicKey(),
+      pubkey: userPublicKey,
     };
+
+    console.log("event", event);
 
     event.id = getEventHash(event);
     event = await window.nostr.signEvent(event);
@@ -104,62 +106,68 @@ export default function CreateBounty() {
 
   return (
     <main className="mx-auto max-w-3xl pb-24 pt-10">
-      <div className="px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">Create Bounty</h1>
-        <div className="mt-8">
-          <h2 className="font-semibold text-gray-800 dark:text-gray-100">Title</h2>
-          <input
-            type="text"
-            value={title}
-            onChange={handleTitleChange}
-            className="mt-4 w-full rounded border border-gray-300 bg-white p-2 text-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
-            placeholder="Bounty title"
-          />
-          <h2 className="pb-4 pt-8 font-semibold text-gray-800 dark:text-gray-100">Content</h2>
-          <MdEditor
-            value={content}
-            config={{
-              view: {
-                menu: true,
-                md: true,
-                html: false,
-              },
-            }}
-            style={{
-              width: "100%",
-              height: "500px",
-            }}
-            renderHTML={(text) => mdParser.render(text)}
-            onChange={handleEditorChange}
-          />
+      {userPublicKey ? (
+        <div className="px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">Create Bounty</h1>
+          <div className="mt-8">
+            <h2 className="font-semibold text-gray-800 dark:text-gray-100">Title</h2>
+            <input
+              type="text"
+              value={title}
+              onChange={handleTitleChange}
+              className="mt-4 w-full rounded border border-gray-300 bg-white p-2 text-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
+              placeholder="Bounty title"
+            />
+            <h2 className="pb-4 pt-8 font-semibold text-gray-800 dark:text-gray-100">Content</h2>
+            <MdEditor
+              value={content}
+              config={{
+                view: {
+                  menu: true,
+                  md: true,
+                  html: false,
+                },
+              }}
+              style={{
+                width: "100%",
+                height: "500px",
+              }}
+              renderHTML={(text) => mdParser.render(text)}
+              onChange={handleEditorChange}
+            />
 
-          <h2 className="pt-8 font-semibold text-gray-800 dark:text-gray-100">Reward (sats)</h2>
-          <input
-            type="text"
-            value={reward}
-            onChange={handleRewardChange}
-            className="mt-4 w-full rounded border border-gray-300 bg-white p-2 text-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
-            placeholder="Value in sats"
-          />
-          {/* TODO: handle tags */}
-          {/* <h2 className="text-gray-100 pt-8">Tags</h2> */}
-          {/* <input */}
-          {/*   type="text" */}
-          {/*   value={inputValue} */}
-          {/*   onChange={handleInputChange} */}
-          {/*   className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2" */}
-          {/*   placeholder="Comma separated tags" */}
-          {/* /> */}
-          <div className="mt-6 flex items-center justify-between">
-            <button
-              className="flex items-center gap-x-2 rounded-lg bg-indigo-500 px-3 py-2 text-sm font-medium text-white hover:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-500"
-              onClick={handlePublish}
-            >
-              Create Bounty
-            </button>
+            <h2 className="pt-8 font-semibold text-gray-800 dark:text-gray-100">Reward (sats)</h2>
+            <input
+              type="text"
+              value={reward}
+              onChange={handleRewardChange}
+              className="mt-4 w-full rounded border border-gray-300 bg-white p-2 text-gray-800 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100"
+              placeholder="Value in sats"
+            />
+            {/* TODO: handle tags */}
+            {/* <h2 className="text-gray-100 pt-8">Tags</h2> */}
+            {/* <input */}
+            {/*   type="text" */}
+            {/*   value={inputValue} */}
+            {/*   onChange={handleInputChange} */}
+            {/*   className="mt-4 w-full rounded border border-gray-600 bg-gray-700 text-gray-100 p-2" */}
+            {/*   placeholder="Comma separated tags" */}
+            {/* /> */}
+            <div className="mt-6 flex items-center justify-between">
+              <button
+                className="flex items-center gap-x-2 rounded-lg bg-indigo-500 px-3 py-2 text-sm font-medium text-white hover:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-500"
+                onClick={handlePublish}
+              >
+                Create Bounty
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="mt-20 flex justify-center">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">Please login to create a bounty.</h1>
+        </div>
+      )}
     </main>
   );
 }
