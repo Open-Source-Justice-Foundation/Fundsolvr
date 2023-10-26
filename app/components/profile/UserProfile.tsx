88dd6777dbcb27c119bd9cda0ad9244fc05ee950
Christian Chiarulli <chris.machine@pm.me>
2023-10-25 22:38:12 -0400

    feat: store profiles by event

--- app/components/profile/UserProfile.tsx
+++ app/components/profile/UserProfile.tsx
@@ -78,11 +78,7 @@ export default function UserProfile() {
       <UserMenu>
         {currentProfile && (
           <img
-            className="
-
-mt-2 inline-block h-10 w-10 rounded-full shadow-lg shadow-gray-800/10 ring-1
-
-          ring-gray-900/10 backdrop-blur hover:bg-gray-50 dark:ring-white/10 dark:hover:bg-gray-800/90"
+            className="mt-2 inline-block h-10 w-10 rounded-full shadow-lg shadow-gray-800/10 ring-1 ring-gray-900/10 backdrop-blur hover:bg-gray-50 dark:ring-white/10 dark:hover:bg-gray-800/90"
             src={currentProfile.picture}
             alt=""
           />
--- app/u/[npub]/page.tsx
+++ app/u/[npub]/page.tsx
@@ -5,10 +5,9 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { getITagValues, verifyGithub } from "@/app/lib/utils";
+import { getITagValue, getITagValues, parseProfileContent, verifyGithub } from "@/app/lib/utils";
 import { useProfileStore } from "@/app/stores/profileStore";
 import { useRelayStore } from "@/app/stores/relayStore";
-import { Profile } from "@/app/types";
 import { PaperAirplaneIcon, UserPlusIcon } from "@heroicons/react/20/solid";
 import { nip19 } from "nostr-tools";
 import type { Event } from "nostr-tools";
@@ -17,9 +16,10 @@ import Timeline from "./Timeline";
 
 export default function UserProfilePage() {
   const { subscribe, relayUrl } = useRelayStore();
-  const { getProfile, setProfile } = useProfileStore();
+  const { getProfileEvent, setProfileEvent } = useProfileStore();
 
   const [publicKey, setpublicKey] = useState<string>("");
+  const [githubVerified, setGithubVerified] = useState(false);
 
   const pathname = usePathname();
   let npub: string = "";
@@ -32,6 +32,7 @@ export default function UserProfilePage() {
   async function verifyGithubForUser(tag: any) {
     const githubUserVerified = await verifyGithub(npub, tag[2]);
     if (githubUserVerified) {
+      setGithubVerified(true);
       return tag[1];
     } else {
       return "";
@@ -39,7 +40,6 @@ export default function UserProfilePage() {
   }
 
   async function setupProfile(event: any) {
-    const profileContent = JSON.parse(event.content);
     let github = "";
     const iTags = getITagValues(event.tags);
 
@@ -50,21 +50,7 @@ export default function UserProfilePage() {
       }
     }
 
-    const profile: Profile = {
-      relay: relayUrl, // Note: relayUrl isn't defined in your provided code
-      publicKey: event.pubkey,
-      about: profileContent.about,
-      lud06: profileContent.lud06,
-      lud16: profileContent.lud16,
-      name: profileContent.name,
-      nip05: profileContent.nip05,
-      picture: profileContent.picture,
-      banner: profileContent.banner,
-      website: profileContent.website,
-      github: github,
-    };
-
-    setProfile(profile); // Note: setProfile isn't defined in your provided code
+    setProfileEvent(relayUrl, event.pubkey, event);
   }
 
   useEffect(() => {
@@ -72,7 +58,11 @@ export default function UserProfilePage() {
       const publicKey: any = nip19.decode(npub).data;
       setpublicKey(publicKey);
 
-      if (getProfile(relayUrl, publicKey)) {
+      if (getProfileEvent(relayUrl, publicKey)) {
+        const github = getITagValue(getProfileEvent(relayUrl, publicKey)?.tags, "github");
+        if (github) {
+          setGithubVerified(true);
+        }
         return;
       }
 
@@ -97,12 +87,12 @@ export default function UserProfilePage() {
         <div className="flex w-full items-center justify-between">
           <img
             className="h-28 w-28 cursor-pointer rounded-full ring-2 ring-white dark:ring-gray-300"
-            src={getProfile(relayUrl, publicKey)?.picture}
+            src={parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).picture}
           />
           <div className="flex gap-x-4">
             <Link
               className="flex items-center justify-center rounded-lg bg-gray-400 px-3 text-white hover:bg-gray-500 dark:bg-gray-700/80 dark:hover:bg-gray-700"
-              href={`/messages/${nip19.npubEncode(getProfile(relayUrl, publicKey)?.publicKey || "")}`}
+              href={`/messages/${nip19.npubEncode(publicKey)}`}
             >
               <PaperAirplaneIcon className="h-5 w-5" />
             </Link>
@@ -116,24 +106,35 @@ export default function UserProfilePage() {
 
         <div className="flex w-full items-center gap-x-4">
           <div className="flex flex-col gap-y-2">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{getProfile(relayUrl, publicKey)?.name}</h2>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).name}
+            </h2>
             <div className="flex items-center gap-x-2">
-              {getProfile(relayUrl, publicKey)?.nip05 && (
+              {parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).nip05 && (
                 <>
-                  <span className="text-gray-500 dark:text-gray-400">{getProfile(relayUrl, publicKey)?.nip05}</span>
+                  <span className="text-gray-500 dark:text-gray-400">
+                    {parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).nip05}
+                  </span>
                   <span className="text-gray-500 dark:text-gray-400">•</span>
                 </>
               )}
-              <span className="text-gray-500 dark:text-gray-400">{getProfile(relayUrl, publicKey)?.lud16}</span>
+              <span className="text-gray-500 dark:text-gray-400">
+                {parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).lud16}
+              </span>
             </div>
           </div>
         </div>
 
         <span className="max-w-sm break-words text-lg text-gray-700 dark:text-gray-100 md:max-w-xl">
-          {getProfile(relayUrl, publicKey)?.about}
+          {parseProfileContent(getProfileEvent(relayUrl, publicKey)?.content).about}
         </span>
-        {getProfile(relayUrl, publicKey)?.github && (
-          <a target="_blank" rel="nofollow noopener noreferrer" href={`https://github.com/${getProfile(relayUrl, publicKey)?.github}`}>
+
+        {githubVerified && (
+          <a
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            href={`https://github.com/${getITagValue(getProfileEvent(relayUrl, publicKey)?.tags, "github")}`}
+          >
             <svg className="h-8 w-8 fill-gray-400" width="24" height="24" viewBox="0 0 24 24">
               <path d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z" />
             </svg>
