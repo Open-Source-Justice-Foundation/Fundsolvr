335392f7f7b99811d8e82c2cb1b0c482a27ae855
Brian C <brianjcarroll0@gmail.com>
2023-11-08 09:24:19 -0500

    feat: backup keys component

---
+++ app/components/BackupKeysButtons.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import { type Event, Filter, getSignature, nip19 } from "nostr-tools";
+import { Octokit } from "octokit";
+
+import { getITagValues, shortenHash, verifyGithub } from "../lib/utils";
+import Avatar from "../messages/components/Avatar";
+import { useRelayStore } from "../stores/relayStore";
+import { useUserProfileStore } from "../stores/userProfileStore";
+import { Profile } from "../types";
+
+interface Props {
+  publicKey?: string;
+  privateKey?: string;
+}
+
+export default function BackupKeys({ publicKey, privateKey }: Props) {
+  const { userPrivateKey, userPublicKey } = useUserProfileStore();
+  const [privateKeyCopyText, setPrivateKeyCopyText] = useState("Copy");
+  const [publicKeyCopyText, setPublicKeyCopyText] = useState("Copy");
+
+  const copyTextToClipboard = async (text: string) => {
+    if ("clipboard" in navigator) {
+      await navigator.clipboard.writeText(text);
+    } else {
+      document.execCommand("copy", true, text);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4 font-['Avenir']">
+      <label htmlFor="publickey" className="flex font-medium text-gray-900 dark:text-gray-100">
+        Public Key
+      </label>
+      <div className="relative">
+        <input
+          type="text"
+          name="publickey"
+          id="publickey"
+          spellCheck="false"
+          value={publicKey ? nip19.npubEncode(publicKey) : nip19.npubEncode(userPublicKey)}
+          onChange={(e) => e.preventDefault()}
+          onClick={async () => {
+            await copyTextToClipboard(publicKey ? nip19.npubEncode(publicKey) : nip19.npubEncode(userPublicKey));
+            setPublicKeyCopyText("Copied");
+            setPrivateKeyCopyText("Copy");
+          }}
+          className="w-full cursor-pointer rounded border border-gray-300 p-2 caret-transparent dark:border-gray-600 dark:bg-gray-600 dark:text-gray-100"
+        />
+
+        <div className="right-0 top-0 flex h-full w-full items-center justify-center lg:absolute lg:items-end lg:justify-end">
+          <button
+            onClick={async () => {
+              await copyTextToClipboard(publicKey ? nip19.npubEncode(publicKey) : nip19.npubEncode(userPublicKey));
+              setPublicKeyCopyText("Copied");
+              setPrivateKeyCopyText("Copy");
+            }}
+            className="flex h-full w-full items-center justify-center px-4 py-2 active:translate-y-1 dark:text-white hover:dark:text-gray-300 lg:justify-end lg:py-0"
+          >
+            {publicKeyCopyText}
+          </button>
+        </div>
+      </div>
+      <label htmlFor="privatekey" className="flex font-medium text-gray-900 dark:text-gray-100">
+        Private Key
+      </label>
+      <div className="relative">
+        <input
+          type="password"
+          name="privatekey"
+          id="privatekey"
+          spellCheck="false"
+          value={privateKey ? nip19.nsecEncode(privateKey) : nip19.nsecEncode(userPrivateKey)}
+          onClick={async () => {
+            await copyTextToClipboard(privateKey ? nip19.nsecEncode(privateKey) : nip19.nsecEncode(userPrivateKey));
+            setPrivateKeyCopyText("Copied");
+            setPublicKeyCopyText("Copy");
+          }}
+          onChange={(e) => e.preventDefault()}
+          className="w-full cursor-pointer rounded border border-gray-300 p-2 caret-transparent dark:border-gray-600 dark:bg-gray-600 dark:text-gray-100"
+        />
+        <div className="right-0 top-0 flex h-full w-full items-center justify-center lg:absolute lg:items-end lg:justify-end">
+          <button
+            onClick={async () => {
+              await copyTextToClipboard(privateKey ? nip19.nsecEncode(privateKey) : nip19.nsecEncode(userPrivateKey));
+              setPrivateKeyCopyText("Copied");
+              setPublicKeyCopyText("Copy");
+            }}
+            className="flex h-full w-full items-center justify-center px-4 py-2 active:translate-y-1 dark:text-white hover:dark:text-gray-300 lg:justify-end lg:py-0"
+          >
+            <span>{privateKeyCopyText}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/menus/BackupKeysMenu.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+
+import { useUserProfileStore } from "../../stores/userProfileStore";
+import BackupKeysButtons from "../BackupKeysButtons";
+
+interface Props {
+  onClick?: Function;
+}
+
+export default function BackupKeysMenu({ onClick }: Props) {
+  const { userPrivateKey } = useUserProfileStore();
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div className="container mx-auto w-full max-w-4xl px-4 lg:px-0">
+      {userPrivateKey ? (
+        <div>
+          <h2 className="text-2xl dark:text-white">Backup Private Key</h2>
+          <div className="mt-4">
+            <BackupKeysButtons></BackupKeysButtons>
+          </div>
+          <div className="mt-6 dark:text-white">
+            <div
+              onClick={(e) => {
+                setChecked(!checked);
+              }}
+              className="mr-auto flex w-fit cursor-pointer select-none flex-row items-center gap-x-2 py-2 pr-2"
+            >
+              <input
+                onClick={(e) => {
+                  setChecked(!checked);
+                }}
+                name="storedkeys"
+                id="storedkeys"
+                type="checkbox"
+                checked={checked}
+              />
+              <label
+                onClick={(e) => {
+                  e.preventDefault();
+                  setChecked(!checked);
+                }}
+                htmlFor="storedkeys"
+                className="cursor-pointer"
+              >
+                I have stored my keys
+              </label>
+            </div>
+            <button
+              disabled={!checked}
+              className="mt-4 w-32 rounded-3xl bg-indigo-500 px-2 py-4 text-sm font-medium text-white hover:bg-indigo-600 disabled:cursor-not-allowed disabled:bg-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-500 disabled:dark:bg-indigo-300"
+              onClick={(e) => {
+                e.preventDefault();
+                if (onClick) {
+                  onClick();
+                }
+              }}
+            >
+              Got it
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="dark:text-white">You must Login or create an account!</div>
+        </div>
+      )}
+    </div>
+  );
+}
