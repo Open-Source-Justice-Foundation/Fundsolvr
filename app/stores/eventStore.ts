2cffb9105c4d844e399e609c9a647cf1709fd81b
Brian C <brianjcarroll0@gmail.com>
2023-12-08 18:54:31 -0500

    Fix enum in event store

--- app/stores/eventStore.ts
+++ app/stores/eventStore.ts
@@ -2,8 +2,6 @@ import type { Event } from "nostr-tools";
 import { create } from "zustand";
 import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
-import { BountyTab } from "../lib/constants";
-
 interface BountyEventState {
   bountyEvents: Record<string, Array<Event>>;
   setBountyEvents: (key: string, bountyEvents: Array<Event>) => void;
@@ -56,8 +54,8 @@ interface BountyEventState {
   getZapReceiptEvent: (relayUrl: string, bountyId: string) => Event | null;
   getZapReceiptEvents: (relayUrl: string) => Record<string, Event>;
 
-  bountyType: BountyTab;
-  setBountyType: (bountyType: BountyTab) => void;
+  bountyType: "all" | "userPosted" | "assigned" | "disputed";
+  setBountyType: (bountyType: "all" | "userPosted" | "assigned" | "disputed") => void;
 }
 
 export const useBountyEventStore = create<BountyEventState>()(
